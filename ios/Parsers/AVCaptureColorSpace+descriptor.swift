e33516a753c5d128da1ebfa042b4c5d87e33fed2
Ryan Kuhn <ryan@suitable.co>
2023-09-27 10:09:07 -0400

    attempt fix

--- ios/Parsers/AVCaptureColorSpace+descriptor.swift
+++ ios/Parsers/AVCaptureColorSpace+descriptor.swift
@@ -24,6 +24,13 @@ extension AVCaptureColorSpace {
     case "srgb":
       self = .sRGB
       return
+    case "appleLog":
+        if #available(iOS 17, *) {
+          self = .appleLog
+        } else {
+          throw EnumParserError.unsupportedOS(supportedOnOS: "17")
+        }
+        return
     default:
       throw EnumParserError.invalidValue
     }
@@ -37,6 +44,8 @@ extension AVCaptureColorSpace {
       return "p3-d65"
     case .sRGB:
       return "srgb"
+    case .appleLog:
+      return "appleLog"
     default:
       fatalError("AVCaptureDevice.Position has unknown state.")
     }
